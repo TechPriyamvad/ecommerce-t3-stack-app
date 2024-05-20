6348a4d0694d8875ee6cc2604c432b102602bd03
Technical Priyam <priyamvadofficial1@gmail.com>
2024-05-21 04:57:51 +0530

    reformatter entry point page code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
-import CategoriesPage from "./categories";
 import SignUpPage from "./signup";
-import SignUpVerificationPage from "./emailVerification";
-import LoginPage from "./login";
 
 export default function Home() {
   return (
-    <>
-      {/* <SignUpPage /> */}
-      {/* <SignUpVerificationPage /> */}
-      <LoginPage />
-      {/* <CategoriesPage /> */}
-    </>
+    <SignUpPage />
   );
 }
